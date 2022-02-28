91acfc1a37672d01e768a6f5be38edd27a5a5e0a
Boccia <fboccia96@gmail.com>
2022-02-28 17:44:45 +0000

    Struggling with the animation

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const containerButton = document.querySelector(".containerButton");
 const buttonTransfomr = document.querySelector(".button");
-const arrowSpan = document.querySelectorAll("span");
+const arrowButton = document.querySelectorAll("button");
 
-arrowSpan.forEach((span, i) =>
+arrowButton.forEach((span, i) =>
   span.addEventListener("click", function () {
-    containerButton.style.backgroundColor = "red";
-    containerButton.style.transition = "4s ease-in-out";
+    containerButton.style.transition = "1s ease";
     containerButton.style.transform = "translate(100%)";
+    if (i === 2) {
+      containerButton.style.transform = "translate(200%)";
+    }
+    if (i === 1) {
+      containerButton.style.transform = "translate(300%)";
+    }
+    if (i === 0) {
+      containerButton.style.transition = ".1s ease";
+      containerButton.style.transform = "translate(0%)";
+    }
+    console.log(i);
   })
 );
