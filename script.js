4e0ac090bb1fb3be5718c57ef1d39f52bce88fa2
Boccia <fboccia96@gmail.com>
2022-03-01 02:46:51 +0000

    There is some issue with the animation

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const containerButton = document.querySelector(".containerButton");
-const buttonTransfomr = document.querySelector(".button");
 const arrowButton = document.querySelectorAll("button");
+const animationWave = document.querySelector(".wave");
 
 arrowButton.forEach((span, i) =>
   span.addEventListener("click", function () {
-    containerButton.style.transition = "1s ease";
-    containerButton.style.transform = "translate(100%)";
+    animationWave.classList.add("wave-animation");
+    setTime();
+    // setTimeout(setTime, 2000);
+    // containerButton.style.transition = "1s ease";
+    // containerButton.style.transform = "translate(100%)";
     if (i === 2) {
       containerButton.style.transform = "translate(200%)";
     }
@@ -19,3 +22,10 @@ arrowButton.forEach((span, i) =>
     console.log(i);
   })
 );
+
+function setTime() {
+  containerButton.style.transition = "1s ease";
+  containerButton.style.transform = "translate(100%)";
+}
+
+// z-index: 1; of button and button content
